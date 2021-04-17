adb0df8897ae1ba394ff663e1c5235b21ab25e75
unknown <nachappapr1991@gmail.com>
2021-04-17 16:17:56 +0530

    added localizied content

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,9 +1,26 @@
 const axios = require("axios");
+const countries = require("./countries.json");
+
 require("dotenv").config();
 
-module.exports = async function getUser() {
-  const response = await axios.get(
-    `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY}&pageSize=5`
-  );
-  return response.data;
+async function getNews(country) {
+  try {
+    const response = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.API_KEY}&pageSize=5`
+    );
+    return {
+      country: country,
+      articles: response.data.articles,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = async function () {
+  var newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then((newsObjects) => {
+    // console.log("newsObjects:", newsObjects);
+    return [].concat.apply([], newsObjects);
+  });
 };
